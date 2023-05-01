6604d46d43e3fae58088b673c47c95b9b0ac0ed7
@Nickbis <nickbis3@gmail.com>
2023-05-02 01:51:06 +0300

    fix: text and color

--- assets/js/header.js
+++ assets/js/header.js
@@ -4,11 +4,11 @@ class Header extends Creater {
   constructor(parent) {
     super(parent, 'div', 'header');
     this.title = new Creater(this.element, 'h1', 'title');
-    this.title.element.textContent = 'Virtual keyboard';
+    this.title.element.textContent = 'RSS virtual keyboard';
     this.subtitle = new Creater(this.element, 'h2', 'subtitle');
-    this.subtitle.element.textContent = 'windows version';
+    this.subtitle.element.textContent = 'Windows version';
     this.subtitle_second = new Creater(this.element, 'h3', 'subtitle-second');
-    this.subtitle_second.element.textContent = 'Change lang: Alt + Ctr';
+    this.subtitle_second.element.textContent = 'To switch the language combination: left Alt + Ctr';
     }
 }
 
